c5c53f03dc42ed8f4078d6cfb7023f79dd02b9f3
Michael Taylor <michaelxtaylor@gmail.com>
2026-01-01 14:31:19 -0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,15 @@
+// apps.js
+const apps = [
+    {
+        name: "Days at the Office",
+        description: "Track your days at the office automatically.",
+        iconUrl: "days-at-the-office/favicon.png", // Placeholder
+        folderName: "days-at-the-office"
+    },
+    {
+        name: "Days in State",
+        description: "Make sure you are state tax compliant by tracking your days in each state.",
+        iconUrl: "days-in-state/icon.png", // Placeholder
+        folderName: "days-in-state"
+    }
+];
